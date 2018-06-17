a3abfffb908fc3fbbe45157c75783e52dbefa18d
rahulbrungi <12682649+rahulbrungi@users.noreply.github.com>
2018-06-17 10:33:59 -0500

    Add files via upload

---
+++ str.java
@@ -0,0 +1,24 @@
+
+public class str {
+	
+	public static void main (String[] args) {
+		
+		String s = "Hello world";
+		String t = "Testing";
+		int len = s.length();
+		char result = s.charAt(8);
+		int a = s.compareTo(t);
+		
+		
+		
+		
+		
+		System.out.println(len);
+		System.out.println(result);
+		System.out.println(a);
+		
+	}
+	
+	
+
+}
